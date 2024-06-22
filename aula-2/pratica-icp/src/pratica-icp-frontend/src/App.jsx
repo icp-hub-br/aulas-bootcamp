607fb845cae268b984f3113446decffee3381745
BryanBorck <bryanborck@gmail.com>
2024-06-22 13:17:02 -0300

    adj

--- aula-2/pratica-icp/src/pratica-icp-frontend/src/App.jsx
+++ aula-2/pratica-icp/src/pratica-icp-frontend/src/App.jsx
@@ -1,6 +1,55 @@
 // import { pratica_icp_backend } from "declarations/pratica-icp-backend";
 
 function App() {
+  // Complete as linhas abaixo
+  //
+  // Declare os valores a serem usados
+  //
+
+  async function handleOperation(operation) {
+    const value = parseInt(inputValue, 10);
+    if (isNaN(value) && operation !== "clearall") {
+      alert("Please enter a valid number");
+      return;
+    }
+
+    let response;
+    try {
+      switch (operation) {
+        case "add":
+          response = await icp_aula1_test_backend.add(value);
+          break;
+        case "sub":
+          response = await icp_aula1_test_backend.sub(value);
+          break;
+        case "mul":
+          response = await icp_aula1_test_backend.mul(value);
+          break;
+        case "div":
+          response = await icp_aula1_test_backend.div(value);
+          response = response !== null ? response : "Error";
+          break;
+        case "clearall":
+          await icp_aula1_test_backend.clearall();
+          response = 0;
+          break;
+        default:
+          return;
+      }
+      // Complete essa linha
+      //
+      // setar o resultado aqui
+      //
+    } catch (error) {
+      console.error("Error performing operation:", error);
+      setResult("Error");
+    }
+  }
+
+  useEffect(() => {
+    console.log("Result updated: ", result);
+  }, [result]);
+
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <header className="">
@@ -8,6 +57,7 @@ function App() {
           Precisamos fazer algo aqui
         </h1>
       </header>
+      {/* Faça o frontend se comunicar com as funções em js aqui */}
     </main>
   );
 }
