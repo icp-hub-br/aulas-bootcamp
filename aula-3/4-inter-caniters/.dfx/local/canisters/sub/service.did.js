72563369b69abc7859b0f334b1623d06babf5e37
Luiz <lvj253@gmail.com>
2024-06-26 20:04:59 -0300

    fixing

--- aula-3/4-inter-caniters/.dfx/local/canisters/sub/service.did.js
+++
@@ -1,9 +0,0 @@
-export const idlFactory = ({ IDL }) => {
-  const Counter = IDL.Record({ 'topic' : IDL.Text, 'value' : IDL.Nat });
-  return IDL.Service({
-    'getCount' : IDL.Func([], [IDL.Nat], ['query']),
-    'init' : IDL.Func([IDL.Text], [], ['oneway']),
-    'updateCount' : IDL.Func([Counter], [], ['oneway']),
-  });
-};
-export const init = ({ IDL }) => { return []; };
